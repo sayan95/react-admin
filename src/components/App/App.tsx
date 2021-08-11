1cb2d533868f1e8856655fad0542863001523bd7
sayan95 <sharthakmallik@gmail.com>
2021-08-11 22:44:38 +0530

    Initail commit

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,7 +4,7 @@ import "./App.css";
 const App = () => {
   return (
     <div className='App'>
-      <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+       <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
         <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">
           Company name
         </a>
@@ -33,7 +33,7 @@ const App = () => {
           </div>
         </div>
       </header>
-      <div className="container-fluid">
+       <div className="container-fluid">
         <div className="row">
           <nav
             id="sidebarMenu"
